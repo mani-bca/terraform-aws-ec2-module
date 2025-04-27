c717b67aae803d50adbc056566d9b07e480acbe6
manivasagan <80743457+mani-bca@users.noreply.github.com>
2025-04-27 11:27:51 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,15 @@
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.web_server.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web_server.public_ip
+}
+
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = aws_security_group.web_server_sg.id
+}
